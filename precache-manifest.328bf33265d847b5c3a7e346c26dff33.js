6f72ed2e9b649811ae92b42a74dcb2db7bd4d0fb
Gregory <gregory-starodubcev@yandex.ru>
2019-10-10 20:38:25 +0300

    Updates

---
+++ precache-manifest.328bf33265d847b5c3a7e346c26dff33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf994c1fefa147e63a07f2bd92fd0840",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "caf2ad1ec81553abb41f",
+    "url": "/todos/static/css/main.dc2ecb36.chunk.css"
+  },
+  {
+    "revision": "f0747d58a9cbb234bc98",
+    "url": "/todos/static/js/2.70f61522.chunk.js"
+  },
+  {
+    "revision": "caf2ad1ec81553abb41f",
+    "url": "/todos/static/js/main.a28cb98c.chunk.js"
+  },
+  {
+    "revision": "0568f0c4577b74ed333a",
+    "url": "/todos/static/js/runtime-main.ac531c4d.js"
+  }
+]);
